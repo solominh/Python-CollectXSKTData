291a58804019c53bf480ca8d47d76250c6f43a4d
solominh <hoangminhftu2@gmail.com>
2017-01-10 16:25:47 +0700

    Css selector work for bs4 => only tbody tag not in html tree

--- app.py
+++ app.py
@@ -2,22 +2,20 @@ import bs4
 import requests
 
 
-def get_first_prize_number_from(url):
+def get_first_prized_number_from(url):
     res = requests.get(url)
     res.raise_for_status()
 
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
-    elements = soup.select(
-        '#center-content > div:nth-of-type(7) > div > table > tbody > tr:nth-of-type(11) > td:nth-of-type(2) > em')
-
+    css_selector = '#center-content > div.box-ketqua > div > table > tr:nth-of-type(11) > td:nth-of-type(2) > em'
+    elements = soup.select(css_selector)
     print('Elements =' + str(elements))
-    # return elements[0].text.strip()
-    return 0
+    return elements[0].text.strip()
 
 
 def main():
     url = 'http://xskt.com.vn/ket-qua-xo-so-theo-ngay/tp-hcm-xshcm/6-2-2016.html'
-    number = get_first_prize_number_from(url)
+    number = get_first_prized_number_from(url)
     print('number =' + str(number))
 
 main()
